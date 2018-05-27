f96c43a6da8f5ed7953a373fbc131fa8da210326
Linliuxing <929593448@qq.com>
2018-05-27 11:35:04 +0800

    Initial commit

---
+++ src/main/java/com/lin/SpringbootSpringbatchApplication.java
@@ -0,0 +1,12 @@
+package com.lin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSpringbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSpringbatchApplication.class, args);
+	}
+}
